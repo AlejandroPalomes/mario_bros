822ef81b5f8cfa7dd90f99faabeb60cb11c81eee
Alex Palomes <alejandropalomes@hotmail.com>
2020-05-03 17:58:16 +0200

    resize bug

--- assets/js/display.js
+++ assets/js/display.js
@@ -2,7 +2,9 @@ function display(){
     windowSize = window.innerWidth;
 
     if (windowSize < 800){
-        resizeContainer.style.transform = "scale(" + windowSize/800 +")"
+        resizeContainer.style.transform = "scale(" + windowSize/800 +")";
+    }else {
+        resizeContainer.style.transform = "scale(1)";
     }
 
 }
