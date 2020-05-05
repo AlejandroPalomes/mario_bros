75d1f280b00fe851ea17a498f8de1024ea10f75d
Alex Palomes <alejandropalomes@hotmail.com>
2020-05-05 11:04:18 +0200

    apply translate only to iOS

--- assets/js/display.js
+++ assets/js/display.js
@@ -5,11 +5,17 @@ function display(){
         resizeContainer.style.transform = "scale(" + windowSize/800 +")";
         if (windowSize < 520){
             document.querySelector("#touchControls").classList.remove("hidden");
-            document.querySelector("#main-container").style.transform = "translate(0, -80px)"
+            // document.querySelector("#main-container").style.transform = "translate(0, -60px)";
+            var deviceAgent = navigator.userAgent.toLowerCase();
+            var agentID = deviceAgent.match(/(iphone|ipod|ipad)/);
+            if (agentID) {
+                document.querySelector("#main-container").style.transform = "translate(0, -60px)";
+            }
         }else{
             document.querySelector("#touchControls").classList.add("hidden");
-            document.querySelector("#main-container").style.transform = ""
+            //document.querySelector("#main-container").style.transform = ""
         }
+        console.log(deviceAgent)
     }else {
         resizeContainer.style.transform = "scale(1)";
     }
