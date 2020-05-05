cb2bd898027db5a259dc25c748615e06ff9a8a65
Alex Palomes <alejandropalomes@hotmail.com>
2020-05-05 10:55:55 +0200

    bug remove iOS

--- assets/js/display.js
+++ assets/js/display.js
@@ -5,8 +5,10 @@ function display(){
         resizeContainer.style.transform = "scale(" + windowSize/800 +")";
         if (windowSize < 520){
             document.querySelector("#touchControls").classList.remove("hidden");
+            document.querySelector("#main-container").style.transform = "translate(0, -80px)"
         }else{
             document.querySelector("#touchControls").classList.add("hidden");
+            document.querySelector("#main-container").style.transform = ""
         }
     }else {
         resizeContainer.style.transform = "scale(1)";
